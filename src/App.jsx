8ba0d25a27f1d66a2cea52db267bc1b8c75d2485
GuillermoTD <guillermotd23@gmail.com>
2024-07-26 17:41:17 -0400

    agregado de segunda seccion de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Hero from './components/Hero/Hero';
 import './App.css';
+import Section1 from './components/Section1/Section1';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Hero />
+      <Section1/>
     </>
   );
 }
